78de2538c6490dc6479e6174e61e052ac5f5e7bd
Giovanny Franco <giovannyfranco@mattelsa.net>
2019-03-25 11:45:11 -0500

    Coonect data to firebase collection

---
+++ src/config/fbConfig.js
@@ -0,0 +1,17 @@
+  import firebase from 'firebase/app'
+  import 'firebase/firestore'
+  import 'firebase/auth'
+  
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAYlrSjA5_dC8-iFZc1Qh0bYSDewDXeY9w",
+    authDomain: "react-firebase-344ca.firebaseapp.com",
+    databaseURL: "https://react-firebase-344ca.firebaseio.com",
+    projectId: "react-firebase-344ca",
+    storageBucket: "",
+    messagingSenderId: "900171567018"
+  };
+  firebase.initializeApp(config);
+  firebase.firestore();
+
+  export default firebase;
