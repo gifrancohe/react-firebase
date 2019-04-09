d7052cd6eb3f953d31c17999e6e489ee29d40470
Giovanny Franco <giovannyfranco@mattelsa.net>
2019-04-09 17:01:17 -0500

    Tracking Auth Status

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -54,4 +54,4 @@ const mapDispatchToProps = (dispatch) => {
     }
 }
 
-export default connect( mapStateToProps ,mapDispatchToProps )(SignIn)
+export default connect( mapStateToProps, mapDispatchToProps )(SignIn)
